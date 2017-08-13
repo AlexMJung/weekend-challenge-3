f06c0fabcb2980cd3dba064bc682090634392c13
Alex Jung <alexandermjung89@gmail.com>
2017-08-13 10:51:59 -0500

    delete function working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
+    getTasks()//calling the current tasks on document load.
 
-getTasks()//calling the current tasks on document load.
-
-
-    //create new task button
+    //creates a new task on the button click
     $('#createTask').on('click', function () {
         console.log('button clicked, woot!!!');
         var newTask = {
@@ -12,37 +10,72 @@ getTasks()//calling the current tasks on document load.
         console.log(newTask.task);
         $.ajax({
             method: 'POST',
-            url: '/tasks.js',
+            url: '/tasks',
             data: newTask,
             success: function (response) {
                 console.log('new task posted')
                 getTasks();
             }//end of success
-        })//ajax
-    })//end of on click function
+        });//ajax
+    });//end of on click function
+
+    //marks a task as complete when complete? button is clicked
+    //*****NOT DONE */
+    $('#toDo').on('click', '.done', function () {
+        console.log('complete button was clicked, holla!!!');
+
+        //$.ajax({
+        //     method: 'POST',
+        //     url: '/tasks',
+        //     data: newTask,
+        //     success: function (response) {
+        //         console.log('new task posted')
+        //         getTasks();
+        //     }//end of success
+        // });//ajax
+    });//end of on click function
+
+    //removes a task completly when delete button is clicked
+    $('#toDo').on('click', '.delete', function () {
+        //console.log('delete button was clicked, woop woop!!!');
+        var deleteId = $(this).parent().parent().data().id;
+        console.log('deleteId =', deleteId);
+        $.ajax({
+            method: 'DELETE',
+            url: '/tasks/' + deleteId,
+            success: function (response) {
+                console.log (response)
+                console.log('new task deleted');
+                getTasks();
+            }//end of success
+        });//ajax
+    });//end of on click function
+
+
+});// end of document.ready
 
 //function to retrieve tasks
-    function getTasks() {
-        $.ajax({
-            method: 'GET',
-            url: '/tasks.js',
-            success: function (response) {
-                for (var i = 0; i < response.length; i++) {
-                    var task = response[i];
-                    $('#toDoItem').append('<li>' +
-                        '<div class=\'toDo\'>' + task.task +
-                        '<button class="done">Complete?</button>' +
-                        '<button class="delete">delete</button>' +
-                        '</div>' +
-                        '</li>');
-                }//end of for loop
-            }//end of response
-        })//end of ajax
-    }//end of getTasks
+function getTasks() {
+    emptyTasks();
+    $.ajax({
+        method: 'GET',
+        url: '/tasks',
+        success: function (response) {
+            console.log(response);
+            for (var i = 0; i < response.length; i++) {
+                var task = response[i];
+                $('#toDoItem').append('<li data-id=' + task.id +'>' +
+                    '<input class="done" type="checkbox">' +
+                    '<span class=\'toDo\'>' + task.task +
+                    '<button class="delete">delete</button>' +
+                    '</span>' +
+                    '</li>');
+            }//end of for loop
+        }//end of response
+    })//end of ajax
+}//end of getTasks
 
-    //function to clear tasks.
-    function emptyTasks() {
-        $('#toDoItem').empty();
-    }//end of emptyTasks
-
-})// end of document.ready
+//function to clear tasks.
+function emptyTasks() {
+    $('#toDoItem').empty();
+}//end of emptyTasks
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -20,9 +20,49 @@ router.get('/', function (req, res) {
             })//end of client.query   
         }//end of 1st child else statement
     })//end of pool.connect
-
 })//end of router.get
 
+//server connection to post a new task
+router.post('/', function (req, res) {
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query("INSERT INTO todo (task) VALUES ('"+ req.body.task +"');" , function (errorMakingQuery, result) {
+                done();//putting the connection back in the pool
+                if (errorMakingQuery) {
+                    console.log('there was an error Making the query(syntax error most likly):', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                }//end of succesful connection else
+            })//end of client.query   
+        }//end of 1st child else statement
+    })//end of pool.connect
+})//end of router.post
 
+router.delete('/:id', function (req, res) {
+    var inputId = req.params.id;
+    console.log(inputId);
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query("DELETE from todo where id = $1;", 
+                [inputId], 
+                function (errorMakingQuery, result) {
+                done();//putting the connection back in the pool
+                if (errorMakingQuery) {
+                    console.log('there was an error Making the query(syntax error most likly):', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(201);
+                }//end of succesful connection else
+            })//end of client.query   
+        }//end of 1st child else statement
+    })//end of pool.connect
+})//end of router.delete
 
 module.exports = router
